ddb40c7e896af9c41f3a645b7b038ed21c928513
Andrei Beliakov <andrei@imd.ru>
2023-03-01 10:34:29 +0300

    fix: fix Cards mapping

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,26 @@
 import { data } from "../data/data.js";
 import { Layout } from "../Layout/Layout";
 import { Wrapper } from "../components/wrapper/Wrapper";
+import { Cards } from "../components/cards/Cards.jsx";
 
 function App() {
-  console.log(data);
   return (
     <>
       <Layout>
-        <Wrapper>Hello! It's Wrapper</Wrapper>
+        <Wrapper>
+          {data.map((el, i) => (
+            <Cards
+              id={i}
+              image={el.image}
+              name={el.name}
+              actor={el.actor}
+              gender={el.gender}
+              house={el.house}
+              wand={el.wand}
+              alive={el.alive}
+            />
+          ))}
+        </Wrapper>
       </Layout>
     </>
   );
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,6 +1,29 @@
 import React from "react";
 import s from "./Cards.module.css";
 
-export const Cards = () => {
-  return <div>Cards</div>;
+export const Cards = ({
+  id,
+  image,
+  name,
+  actor,
+  gender,
+  house,
+  wand,
+  alive,
+}) => {
+  return (
+    <>
+      <div className={s.cards__item} key={id}>
+        <img src={image} alt="" />
+        <div className={s.item__text}>
+          <p className={s.item__title}>{name}</p>
+          <p className={s.item__data}>Actor: {actor}</p>
+          <p className={s.item__data}>Gender: {gender}</p>
+          <p className={s.item__data}>House: {house}</p>
+          <p className={s.item__data}>Wand core: {wand.core}</p>
+          <p className={s.item__data}>Alive: {alive}</p>
+        </div>
+      </div>
+    </>
+  );
 };
